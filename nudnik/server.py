f4f4dadc784d57340645667c51607773a8c79072
Saloshp <saloshp@gmail.com>
2018-12-30 19:44:56 +0000

    Added remote cmd via meta support

--- nudnik/server.py
+++ nudnik/server.py
@@ -44,7 +44,7 @@ class ParseService(nudnik.entity_pb2_grpc.ParserServicer):
 
         # Generate fake load for incoming request
         for load in self.cfg.load_list:
-            utils.generate_load(self.log, load)
+            utils.generate_load(self.log, load, request.meta)
 
         ltime = utils.time_ns()
 
@@ -76,7 +76,7 @@ class ParseService(nudnik.entity_pb2_grpc.ParserServicer):
             self.stats.add_failure()
             status_code = 'SERVER_ERROR'
 
-        response = {'status_code': status_code, 'ctime': timestamp, 'ltime': ltime, 'stime': utils.time_ns(), 'meta': utils.get_meta(self.cfg)}
+        response = {'status_code': status_code, 'ctime': timestamp, 'ltime': ltime, 'stime': utils.time_ns(), 'meta': utils.get_meta(self.cfg.meta, self.cfg.meta_size)}
         grpc_response = nudnik.entity_pb2.Response(**response)
 
         stat = nudnik.stats.Stat(request, grpc_response, timestamp)
